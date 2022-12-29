def52aea5dbe7e8ba1eacd477edac49dff44b676
valentinferreyraML <valentin.ferreyra@mercadolibre.com>
2022-12-29 14:25:22 -0300

    first commit

---
+++ internal/tickets/tickets_test.go
@@ -0,0 +1,5 @@
+package tickets
+
+import "testing"
+
+func TestGetTotalTickets(t *testing.T) {}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
+)
+
+func main() {
+	total, err := tickets.GetTotalTickets("Brazil")
+}
